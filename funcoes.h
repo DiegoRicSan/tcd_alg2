b91578fc1b1b71329129a54adf98a135be5b2d94
Pedro-Benfica <pedrobenfico59@gmail.com>
2026-03-10 21:48:30 -0300

    Add bubbleSort function declaration to funcoes.h

--- funcoes.h
+++ funcoes.h
@@ -1,6 +1,7 @@
 int carregarArquivo(char *nome_arquivo, int **vetor, int *total);
 int buscaLinear (int *lista, int chave, int tam);
 int buscaBinaria(int *vet, int chave, int inicio, int fim);
+void bubbleSort(int vet[], int tam);
 void insertionSort(int vet[], int tam);
 void selectionSort(int vet[], int tam);
 void mergeSort(int vet[], int inicio, int fim);
